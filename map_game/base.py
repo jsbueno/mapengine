afc43e722f705040ede6d4709c1abf1843c58500
João S. O. Bueno <gwidion@gmail.com>
2015-10-12 21:57:32 -0300

    Basic scene loading

--- map_game/base.py
+++ map_game/base.py
@@ -1,20 +1,42 @@
 # coding: utf-8
 import pygame
 
+from pygame.color import Color
+
+SIZE = 800, 600
 
 class GameOver(Exception):
     pass
 
 
+
 class Controller(object):
-    def __init__(self, SIZE, scene=None, **kw):
-        self.screen = pygame.display.set_mode(SIZE, **kw)
+    def __init__(self, size, scene=None, **kw):
+        self.width, self.height = self.size = size
+        self.screen = pygame.display.set_mode(size, **kw)
         self.scene = scene
 
     def update(self):
         self.scene.update()
+        self.draw()
 
-    def quit():
+    def draw(self):
+        self.background()
+
+    def background(self):
+        scene = self.scene
+        scale = scene.mapscale
+        blocks_x = self.width / scale
+        blocks_y = self.width / scale
+        for x in range(blocks_x):
+            for y in range(blocks_y):
+                image = scene[x + scene.left,y + scene.top]
+                if isinstance(image, Color):
+                    pygame.draw.rect(self.screen, image, (x * scale, y * scale, scale, scale))
+                else: # image
+                    self.screen.blit(image, (x * scale, y * scale))
+
+    def quit(self):
         pygame.quit()
 
 
@@ -27,11 +49,20 @@ class Palette(object):
         self.path = path
         self.colors = {}
         self.color_names = {}
+        self.by_index = {}
         self.load()
 
     def __getitem__(self, key):
         if isinstance(key, str):
             return self.color_names[key]
+        elif isinstance(key, int):
+            if key < 0:
+                key = len(self.by_index) + key
+            return self.by_index[key]
+        if isinstance(key, Color):
+            key = tuple(key)
+        if len(key) == 3:
+            key = key + (255,)
         return self.colors[key]
 
     def __len__(self):
@@ -45,37 +76,62 @@ class Palette(object):
             line = ""
             while not line.strip().startswith('#'):
                 line = next(file_)
+            index = 0
             for line in file_:
                 line = line.strip()
                 if len(line.split()) < 4 or line.startswith('#'):
                     continue
                 r, g, b, name = line.strip().split(None, 4)
-                color = pygame.color.Color(int(component) for component in (r, g, b))
-                self.colors[color] = name
+                color = Color(*(int(component) for component in (r, g, b)))
+                self.colors[tuple(color)] = name
                 self.color_names[name] = color
+                self.by_index[index] = color
+                index += 1
 
 
 class Scene(object):
-    mapfile = "scene0.png"
-    mapdescription = "scene0.gpl"
-    mapscale = 32
+    mapscale = 64
+    scene_path_prefix = "./"
+    out_of_map = Color(0,0,0)
 
-    def __init__(self):
-        self.X = 0
-        self.Y = 0
+    def __init__(self, mapfile, mapdescription, **kw):
+        self.top = 0
+        self.left = 0
+        self.mapfile = mapfile
+        self.mapdescription = mapdescription
+        self.load()
+
+    def load(self):
+        self.image = pygame.image.load(self.scene_path_prefix + self.mapfile)
+        self.palette = Palette(self.scene_path_prefix + self.mapdescription)
+
+    def __getitem__(self, position):
+        try:
+            color = self.image.get_at(position)
+        except IndexError:
+            return self.out_of_map
+        # TODO: load scene block images
+        return color
 
     def update(self):
         pass
 
 
 def main():
-    cont = Controller
+    mapfile = "scene0.png"
+    mapdescription = "scene0.gpl"
+    cont = Controller(SIZE, Scene(mapfile, mapdescription))
     try:
         while True:
+            pygame.event.pump()
             cont.update()
             cont.draw()
             pygame.display.flip()
             pygame.time.delay(30)
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_ESCAPE]:
+                raise GameOver
 
     except GameOver:
         # cont.scene = EndGameScene()
